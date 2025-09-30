3bc425641cecd0fe0a9d8b1a76671a188670a6b7
Qi Qing <learn.qiqing@gmail.com>
2025-09-30 15:56:07 -0400

    added class constructors

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private String serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone(String serialNumber, String model, String carrier, String phoneNumber, String owner) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
+
 }
