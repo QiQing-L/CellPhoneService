aabc0fe14ce45e39618ff01dd76f8c5569365e8a
Qi Qing <learn.qiqing@gmail.com>
2025-10-01 11:54:14 -0400

    added prompted user for input.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -9,15 +9,34 @@ public class CellPhoneApplication {
 
         CellPhone newPhone = new CellPhone();
 
+        System.out.print("\nWhat is the serial number? ");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+        newPhone.setSerialNumber();
+
+        System.out.print("\nWhat model is the Phone? ");
+        String model = scanner.nextLine();
+        newPhone.setModel();
+
+        System.out.println("Who is the carrier?" + newPhone.getCarrier());
+        String carrier = scanner.nextLine();
+        newPhone.setCarrier();
+
+        System.out.println("What is the phone number? " + newPhone.getPhoneNumber());
+        String phoneNumber = scanner.nextLine();
+        newPhone.setPhoneNumber();
+
+        System.out.println("Who is the owner of the phone? " + newPhone.getOwner());
+        String owner = scanner.nextLine();
+        newPhone.setOwner();
+
+
+
         System.out.println("What is the serial number? " + newPhone.getSerialNumber());
         System.out.println("What model is the Phone? " + newPhone.getPhoneNumber());
         System.out.println("Who is the carrier?" + newPhone.getCarrier());
         System.out.println("What is the phone number? " + newPhone.getPhoneNumber());
         System.out.println("Who is the owner of the phone? " + newPhone.getOwner());
-
-
-
-
 
 
 
