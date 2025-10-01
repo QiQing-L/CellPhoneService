46372a4e287f989146393179c27b5eb5b681d593
Qi Qing <learn.qiqing@gmail.com>
2025-10-01 10:31:05 -0400

    added getter and setter, fixed up value type of serialNumber, and fixed to a parameterless constructor.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CellPhone myPhone = new CellPhone();
+
+        System.out.println(myPhone.getPhoneNumber());
+        System.out.println();
+
+        myPhone.setPhoneNumber("");
+        System.out.println(myPhone.getPhoneNumber());
 
     }
 }
